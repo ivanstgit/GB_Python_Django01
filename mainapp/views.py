75dab08b325038dfc0eaf3c659fd86cf0e9a45e5
ivanstgit <100668917+ivanstgit@users.noreply.github.com>
2023-01-12 15:41:21 +0500

    Lesson 3 (#2)
    * Lesson 3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,6 @@
+import json
+from datetime import datetime
+
 from django.views.generic import TemplateView
 
 
@@ -8,6 +11,43 @@ class MainPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+
+        # Create your own data
+        news_dict = {}
+        with open("mainapp/news.json", "r", encoding="utf-8") as news_file:
+            news_dict = json.load(news_file)
+
+        # Read page number and adjust
+        items_per_page = 5
+        pages_count = 1 + len(news_dict.keys()) // items_per_page
+        if self.request.GET.get("page"):
+            page_num = int(self.request.GET.get("page"))
+        else:
+            page_num = 1
+
+        if page_num > pages_count:
+            page_num = pages_count
+
+        # Calculate slices for current page
+        news_from_index = (page_num - 1) * items_per_page
+        news_to_index = page_num * items_per_page - 1
+        if len(news_dict) <= news_to_index:
+            news_to_index = len(news_dict)
+
+        news_list = list(news_dict.values())[news_from_index:news_to_index:1]
+        for item in news_list:
+            item["datetime"] = datetime.strptime(item.get("datetime"), "%Y-%m-%dT%H:%M:%S.%f")
+
+        context["news_list"] = news_list
+        context["page_num"] = page_num
+        context["pages_range"] = range(1, pages_count + 1)
+        context["items_per_page"] = items_per_page
+
+        return context
 
 
 class CoursesPageView(TemplateView):
