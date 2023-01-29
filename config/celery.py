85921b51db8327a8465f1ad1c93f372defb3c287
Ivan Strelkov <ivan.strelkov.git@mail.ru>
2023-01-30 01:24:10 +0500

    lesson_9 Preporations for deployment

--- config/celery.py
+++ config/celery.py
@@ -1,10 +1,11 @@
 import os
 
 from celery import Celery
+from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+if settings.DEBUG:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 celery_app = Celery("braniac")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
-# This scans all directories for task.py files
 celery_app.autodiscover_tasks()
